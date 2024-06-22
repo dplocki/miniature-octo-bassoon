e8915bd7fbbfcc79391bc9dc6c7edc2701e2e933
dplocki <dawid.plocki@gmail.com>
2024-06-22 08:36:49 +0200

    refactor: remove unnessery code (the function wrap) from explain_xkcd

--- explain_xkcd.js
+++ explain_xkcd.js
@@ -1,11 +1,9 @@
-(function() {
-    const url = window.location.href;
-    const regex = /https:\/\/xkcd\.com\/(\d+)\//;
-    const match = url.match(regex);
+const url = window.location.href;
+const regex = /https:\/\/xkcd\.com\/(\d+)\//;
+const match = url.match(regex);
 
-    if (match) {
-        window.location.href = `https://www.explainxkcd.com/wiki/index.php/${match[1]}`;
-    } else {
-        alert('That is not a XKCD page');
-    }
-})();
+if (match) {
+    window.location.href = `https://www.explainxkcd.com/wiki/index.php/${match[1]}`;
+} else {
+    alert('That is not a XKCD page');
+}
