a03fabae55b1ed88adb1fc254488068ac8edb819
dplocki <dawid.plocki@gmail.com>
2024-06-22 07:27:30 +0200

    feat: add explain_xkcd.js

---
+++ explain_xkcd.js
@@ -0,0 +1,11 @@
+(function() {
+    const url = window.location.href;
+    const regex = /https:\/\/xkcd\.com\/(\d+)\//;
+    const match = url.match(regex);
+
+    if (match) {
+        window.location.href = `https://www.explainxkcd.com/wiki/index.php/${match[1]}`;
+    } else {
+        alert('That is not a XKCD page');
+    }
+})();
