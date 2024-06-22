ffb50d8981f13d53a0bd1799cf99e3bb5e5a0eab
dplocki <dawid.plocki@gmail.com>
2024-06-22 09:04:30 +0200

    feat: add documention into the js file

--- explain_xkcd.js
+++ explain_xkcd.js
@@ -1,3 +1,8 @@
+/*
+### Explain XKCD
+
+If you are on [xkcd](https://xkcd.com), it will redirect you on the proper subpage on the [explain xkcd wiki](https://www.explainxkcd.com/). Otherwise display the alert.
+*/
 const url = window.location.href;
 const regex = /https:\/\/xkcd\.com\/(\d+)\//;
 const match = url.match(regex);
