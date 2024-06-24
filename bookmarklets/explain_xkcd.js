299ec8f6807c5aead885eda893b0aa7d3536460c
dplocki <dawid.plocki@gmail.com>
2024-06-24 21:20:11 +0200

    feat: extend description of the 'explain xkcd' bookmarklet

--- bookmarklets/explain_xkcd.js
+++ bookmarklets/explain_xkcd.js
@@ -1,7 +1,7 @@
 /*
 ### Explain XKCD
 
-If you are on [xkcd](https://xkcd.com), it will redirect you on the proper subpage on the [explain xkcd wiki](https://www.explainxkcd.com/). Otherwise display the alert.
+If you are on [xkcd](https://xkcd.com), it will redirect you on the proper subpage on the [explain xkcd wiki](https://www.explainxkcd.com/). Otherwise display the alert informing about the mistake.
 */
 const match = window.location.href.match(/https:\/\/xkcd\.com\/(\d+)\//);
 
