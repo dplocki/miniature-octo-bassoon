99b8dd764a0ed3d24605bb84a0ca6b7a6b3bee09
dplocki <dawid.plocki@gmail.com>
2024-06-24 22:12:06 +0200

    feat: removing typo in the description and replace the article in the message

--- bookmarklets/explain_xkcd.js
+++ bookmarklets/explain_xkcd.js
@@ -1,12 +1,12 @@
 /*
 ### Explain XKCD
 
-If you are on [xkcd](https://xkcd.com), it will redirect you on the proper subpage on the [explain xkcd wiki](https://www.explainxkcd.com/). Otherwise display the alert informing about the mistake.
+If you are on [xkcd](https://xkcd.com), it will redirect you on the proper sub-page on the [explain xkcd wiki](https://www.explainxkcd.com/). Otherwise display the alert informing about the mistake.
 */
 const match = window.location.href.match(/https:\/\/xkcd\.com\/(\d+)\//);
 
 if (match) {
     window.location.href = `https://www.explainxkcd.com/wiki/index.php/${match[1]}`;
 } else {
-    alert('That is not a XKCD page');
+    alert('That is not the XKCD page');
 }
