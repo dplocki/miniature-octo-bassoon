d426c6c49e52279fc27b05df11de87f7e02c4e80
dplocki <dawid.plocki@gmail.com>
2025-01-31 17:50:31 +0100

    feat: change in function generating final output

--- build/build_readme.js
+++ build/build_readme.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-const uglifyJS = require("uglify-js");
+import { readFileSync, readdirSync } from 'fs';
+import { join } from 'path';
+import { minify } from "uglify-js";
 
 function* loadPartOfFile(startToken, endToken, fileName) {
-    const lines = fs.readFileSync(fileName, 'utf8').split('\n');
+    const lines = readFileSync(fileName, 'utf8').split('\n');
 
     let isBlock = !startToken;
     for (const line of lines) {
@@ -24,7 +24,13 @@ function* loadPartOfFile(startToken, endToken, fileName) {
 }
 
 function bookmarklet(bookmarkletFilePath) {
-    return 'javascript:' + encodeURIComponent('(function(){' + uglifyJS.minify(fs.readFileSync(bookmarkletFilePath, "utf8")).code + '})()');
+    const minifyResult = minify(readFileSync(bookmarkletFilePath, "utf8"));
+
+    if (minifyResult.error) {
+        throw new Error(`Minification error: ${minifyResult.error}`);
+    }
+
+    return 'javascript:' + encodeURIComponent(minifyResult.code.trim());
 }
 
 const README_START_MARKER = '## Bookmarklets';
@@ -32,8 +38,8 @@ const bookmarkletsDirectory = '../bookmarklets';
 
 let result = [...loadPartOfFile(null, README_START_MARKER, '../README.md'), README_START_MARKER, ''];
 
-for (const fileName of fs.readdirSync(bookmarkletsDirectory)) {
-    const bookmarkletFilePath = path.join(bookmarkletsDirectory, fileName);
+for (const fileName of readdirSync(bookmarkletsDirectory)) {
+    const bookmarkletFilePath = join(bookmarkletsDirectory, fileName);
 
     result = result.concat([
         ...loadPartOfFile('/*', '*/', bookmarkletFilePath),
