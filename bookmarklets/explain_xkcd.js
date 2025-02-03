d7c64a0c316a85e5fbd52a03c229c911fd3145f9
dplocki <dawid.plocki@gmail.com>
2025-02-03 21:11:05 +0100

    feat(explain_xkcd): add self-invoking-function block

--- bookmarklets/explain_xkcd.js
+++ bookmarklets/explain_xkcd.js
@@ -3,10 +3,12 @@
 
 If you are on [xkcd](https://xkcd.com), it will redirect you on the proper sub-page on the [explain xkcd wiki](https://www.explainxkcd.com/). Otherwise display the alert informing about the mistake.
 */
-const match = window.location.href.match(/https:\/\/xkcd\.com\/(\d+)\//);
+(function () {
+    const match = window.location.href.match(/https:\/\/xkcd\.com\/(\d+)\//);
 
-if (match) {
-    window.location.href = `https://www.explainxkcd.com/wiki/index.php/${match[1]}`;
-} else {
-    alert('That is not the XKCD page');
-}
+    if (match) {
+        window.location.href = `https://www.explainxkcd.com/wiki/index.php/${match[1]}`;
+    } else {
+        alert('That is not the XKCD page');
+    }
+})();
